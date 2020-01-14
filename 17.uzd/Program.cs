5344e9edb39ea104fb4ac59cd880f2554e9dc99e
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:07:24 +0200

    Add project files.

---
+++ 17.uzd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _17.uzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = true;
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine($"{i} {x}");
+                i++;
+            }
+
+            x = false;
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
+
+
+// Deklarēt programmā bool tipa mainīgo, kura sākotnējā vērtība ir TRUE. 
+// Izveidot while ciklu, kurš atkārtojas tik ilgi kamēr mainīgā vērtība ir TRUE. 
+// Mainīgā vērtību uz FALSE ir jānomaina, tad, kad cikls ir atkārtojies 20 reizes.
